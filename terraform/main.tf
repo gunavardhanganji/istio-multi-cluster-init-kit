0dd50efaf4ed91cf0b7edcb187d0bcf9d7d0dd7e
gunavardhanganji <gunavardhan963@gmail.com>
2023-09-03 21:58:25 +0530

    chore: new cluster-2

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,25 @@ module "cluster-1" {
 module "cluster-1-node-pool" {
     source = "./modules/k8s-node-pool"
     node_pool_name = "cluster-1-node-pool"
-    cluster_name = "cluster-1"
+    cluster_name = "projects/istiosetup/locations/us-central1/clusters/cluster-1"
     location = "us-central1-c"
+    machine_type = "n1-standard-2"
+    service_account = "k8s-sa@istiosetup.iam.gserviceaccount.com"
+}
+
+
+module "cluster-2" {
+  source = "./modules/k8s-cluster"
+  cluster_name = "cluster-2"
+  location = "us-east1-b"
+  project_id = "istiosetup"
+}
+
+module "cluster-2-node-pool" {
+    source = "./modules/k8s-node-pool"
+    node_pool_name = "cluster-2-node-pool"
+    cluster_name = "projects/istiosetup/locations/us-east1/clusters/cluster-2"
+    location = "us-east1-b"
     machine_type = "n1-standard-2"
     service_account = "k8s-sa@istiosetup.iam.gserviceaccount.com"
 }
