5b5ac7c016d8d0a1fb69685f0c69af306e64b058
gunavardhanganji <gunavardhan963@gmail.com>
2023-09-03 20:24:57 +0530

    feat: terraform module to create gke and node-pool

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+
+module "cluster-1" {
+  source = "./modules/k8s-cluster"
+  cluster_name = "cluster-1"
+  location = "us-central1-c"
+  project_id = "istio-setup"
+}
+
+module "cluster-1-node-pool" {
+    source = "./modules/k8s-node-pool"
+    node_pool_name = "cluster-1-node-pool"
+    cluster_name = "cluster-1"
+    location = "us-central1-c"
+    machine_type = "n1-standard-2"
+    service_account = "k8s-sa@istiosetup.iam.gserviceaccount.com"
+}
